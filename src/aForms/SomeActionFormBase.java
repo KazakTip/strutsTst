d75926ed67eea772e859a42454586a58bd66ad06
sbt-shumeyko-am <a@g.com>
2017-02-06 11:56:13 +0300

    Kazakov First Commit

---
+++ src/aForms/SomeActionFormBase.java
@@ -0,0 +1,10 @@
+package aForms;
+
+/**
+ * @author SBT-Kazakov-NO
+ * @ created 06.02.2017
+ * @ $Author$
+ * @ $Revision$
+ */
+public class SomeActionFormBase
+{}
---
+++ src/beans/SomeBean.java
@@ -0,0 +1,10 @@
+package beans;
+
+/**
+ * @author sbt-kazakov-no
+ * @ created 03.02.2017
+ * @ $Author$
+ * @ $Revision$
+ */
+public class SomeBean
+{}
